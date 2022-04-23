950d2b6e5fdd00510fe986ecc4b45b30917b647b
creator8080 <98257837+creator8080@users.noreply.github.com>
2022-04-23 07:49:42 +0300

    Update player.py

--- player.py
+++ player.py
@@ -1,7 +1,7 @@
 from multiprocessing import Value
 from re import M
 from PyQt5.QtWidgets import (QApplication, QWidget, QLabel, QPushButton, QLineEdit,QMessageBox,QListWidget,
- QHBoxLayout, QVBoxLayout,QFileDialog,QTextBrowser, QSlider,QListView)
+ QHBoxLayout, QVBoxLayout,QFileDialog,QTextBrowser, QSlider,QListView,QComboBox)
 from PyQt5.QtCore import Qt,QTimer,QStringListModel 
 from PyQt5.QtGui import QPalette,QIcon, QBrush, QPixmap
 from PyQt5.QtMultimedia import QMediaPlayer, QMediaContent
@@ -13,16 +13,20 @@ import ast
 from PyQt5.QtGui import *
 import os
 from os.path import basename
+from PyQt5.QtCore import *
 
 app = QApplication([])
 main = QWidget()
-main.setGeometry(400,300,400,300)
+main.setGeometry(600,500,600,500)
 main.setWindowTitle('MP3-Player')
 app.setWindowIcon(QIcon('Mp3-Player-icon.png'))
 
 m = 0
 s = 0 
 time_total = 0
+min = 0
+sec = 0
+counto = 0
 
 row = QVBoxLayout()
 row1 = QHBoxLayout()
@@ -30,20 +34,37 @@ row2 = QHBoxLayout()
 row3 = QHBoxLayout()
 row4 = QHBoxLayout()
 row5 = QHBoxLayout()
+row6 = QHBoxLayout()
+row7 = QHBoxLayout()
 
 player = QMediaPlayer()
 player_duration = player.duration()
 volumslider = QSlider(Qt.Horizontal)
 volumslider.setFocusPolicy(Qt.NoFocus)
+choose = ''
 volumslider.setValue(0)
 volum = volumslider.value()
+playlists_lst = []
 def change_volume(valum):
     player.setVolume(valum)
     colum_count.setText(str(valum))
 volumslider.valueChanged[int].connect(change_volume)
-with open('for_player.txt','r') as f:
+with open('for_player','r') as f:
     what = f.read()
 lst = ast.literal_eval(what)
+with open('all_playlist.txt','r') as f:
+    whats = f.read()
+playlists_lst = ast.literal_eval(whats)
+if not str(os.path.abspath('for_player.txt')) in playlists_lst:
+    with open('all_playlist.txt','w') as f:
+        p = os.path.abspath('for_player.txt')
+        print(p)
+        playlists_lst.append(p)
+        whats = f.write(str(playlists_lst))
+with open('all_playlist.txt','r') as f:
+    whats = f.read()
+playlists_lst = ast.literal_eval(whats)
+choose = basename(playlists_lst[0])
 now = 0
 player.setMedia(QMediaContent(QUrl('')))
 item_list = lst
@@ -53,9 +74,16 @@ model.setStringList(item_list)
 full.setModel(model)
 plaing = QLabel('Now playing: ')
 plaing_long = QLabel('Long of music: ')
+file_name = QLineEdit()
+add_file_name = QPushButton('Create')
+delete_file_name = QPushButton('Delete')
 play = QPushButton('Play')
+all_play_list = QComboBox()
+for i in playlists_lst:
+    all_play_list.addItems([str(basename(i))])
 next = QPushButton('Next')
 rands = QPushButton('Play Random')
+secret = QPushButton('Hide all songs')
 back = QPushButton('Back')
 reset = QPushButton('Reset')
 radio = QPushButton('Radio')
@@ -73,9 +101,14 @@ row2.addWidget(volumslider)
 row2.addWidget(colum_count)
 row3.addWidget(load)
 row3.addWidget(full)
+row4.addWidget(secret)
 row4.addWidget(rands)
 row5.addWidget(plaing)
 row5.addWidget(plaing_long)
+row6.addWidget(file_name)
+row6.addWidget(add_file_name)
+row7.addWidget(all_play_list)
+row7.addWidget(delete_file_name)
 
 def playMedia():
     if play.text() == 'Play':
@@ -157,13 +190,14 @@ def remove():
     item_list = lst
     model.setStringList(item_list)
     full.setModel(model)
-    with open('for_player.txt','w') as f:
+    with open(choose,'w') as f:
         f.write(str(lst))
 
 def ran():
     global lst
     global now
-    with open('for_player.txt','w') as f:
+    hides()
+    with open(choose,'w') as f:
         shuffle(lst)
         f.write(str(lst))
     now = 0
@@ -171,39 +205,109 @@ def ran():
     model.setStringList(item_list)
     full.setModel(model)
     next_music()
+    hides()
     playMedia()
     playMedia()
 
 def download():
     global lst
     wb_patch = QFileDialog.getOpenFileName()[0]
-    with open('for_player.txt','w') as f:
+    with open(choose,'w') as f:
         lst.append(wb_patch)
         f.write(str(lst))
     item_list = lst
     model.setStringList(item_list)
     full.setModel(model)
 
+def create():
+    global all_play_list
+    name_play_list = file_name.text()
+    if not os.path.abspath(name_play_list + '.txt') in playlists_lst and name_play_list != '':
+        file = open(name_play_list + '.txt', 'a')
+        total = os.path.abspath(name_play_list + '.txt')
+        file_name.setText('')
+        playlists_lst.append(total)
+        with open('all_playlist.txt','w') as f:
+            f.write(str(playlists_lst))
+        all_play_list.clear()
+        for i in playlists_lst:
+            all_play_list.addItems([str(basename(i))])
+        file.close()
+        with open(name_play_list + '.txt','w') as f:
+            f.write('[]')
+
+def stat():
+    global lst
+    global choose
+    global model
+    global full
+    global now
+    if secret.text() == 'Show all songs':
+        secret.setText('Hide all songs') 
+    now = 0
+    if all_play_list.currentText() != '':
+        choose = all_play_list.currentText()
+        with open(choose,'r') as f:
+            need = f.read()
+            lst = ast.literal_eval(need)
+            item_list = lst
+            model.setStringList(item_list)
+            full.setModel(model)
+
+def delete():
+    global choose
+    global counto
+    global all_play_list
+    global playlists_lst
+    for i in playlists_lst:
+        if i != os.path.abspath('for_player.txt'):
+            if i == os.path.abspath(choose):
+                with open('all_playlist.txt','w') as f:
+                    playlists_lst.remove(os.path.abspath(choose))
+                    os.remove(os.path.abspath(choose))
+                    f.write(str(playlists_lst))        
+                all_play_list.removeItem(counto)
+            counto += 1
+        else:
+            pass
+
 def radio_play():
     player.setMedia(QMediaContent(QUrl('http://europaplus.hostingradio.ru:8014/ep-top256.mp3')))
     plaing.setText('Сейчас играет: Радио - Европа плюс')
     playMedia()
     playMedia()
-      
+
+def hides():
+    if secret.text() == 'Hide all songs':
+        item_list = []
+        model.setStringList(item_list)
+        full.setModel(model)
+        plaing.hide()
+        secret.setText('Show all songs')
+    else:
+        stat()
+        plaing.show()
+        secret.setText('Hide all songs')    
 
 full.doubleClicked.connect(remove)
 play.clicked.connect(playMedia)
 back.clicked.connect(backs)
 reset.clicked.connect(resets)
 next.clicked.connect(next_music)
+add_file_name.clicked.connect(create)
+secret.clicked.connect(hides)
 rands.clicked.connect(ran)
 radio.clicked.connect(radio_play)
 load.clicked.connect(download)
+delete_file_name.clicked.connect(delete)
+all_play_list.currentTextChanged.connect(stat)
 row.addLayout(row1)
 row.addLayout(row2)
 row.addLayout(row5)
 row.addLayout(row4)
 row.addLayout(row3)
+row.addLayout(row6)
+row.addLayout(row7)
 main.setLayout(row)
 
 main.show()
